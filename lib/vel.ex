1a3943fb76515ea440b6d882f4f082071025444c
axtelth <axtelth@gmail.com>
2022-03-22 23:34:00 +0300

    uhe[pagnv[9ouie5tg[au8rg[

--- lib/vel.ex
+++ lib/vel.ex
@@ -7,21 +7,18 @@ defmodule Vel do
   @spec main :: no_return
   def main do
     spawn(Longpoll, :start, [self()])
-
     handler()
   end
 
   @spec handler :: no_return
-  def handler do
+  defp handler do
     receive do
       {:ok, response} ->
         updates = response["updates"]
 
         unless updates == nil do
           Enum.each(updates, fn update ->
-            Task.start(fn ->
-              handle_event(update)
-            end)
+            Task.start(fn -> handle_event(update) end)
           end)
         end
     end
@@ -29,15 +26,15 @@ defmodule Vel do
     handler()
   end
 
-  def handle_event([type | [_msg_id, _flags, peer_id, _timestamp, text, meta]]) when type == 4 do
-    unless "title" in meta do
+  defp handle_event([4, _msg_id, _flags, peer_id, _timestamp, text, meta]) do
+    unless meta["title"] do
       handle_message(peer_id, text, meta["from"])
     end
   end
 
-  def handle_event(_), do: nil
+  defp handle_event(_), do: nil
 
-  def handle_message(peer_id, text, _sender) do
+  defp handle_message(peer_id, text, _sender) do
     case text |> String.downcase() do
       "мяу" ->
         %{
